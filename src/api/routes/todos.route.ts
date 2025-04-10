6d167154ece3a020ace3071570ce19368c48c70b
Muhammad Dawood <dawood9743@gmail.com>
2025-04-10 19:03:01 +0500

    added todos in routes as well

---
+++ src/api/routes/todos.route.ts
@@ -0,0 +1,77 @@
+import { NextFunction, Request, Response } from "express";
+
+import express from "express";
+
+const router = express.Router();
+import Todos from "../models/todos.model";
+// import mongoose from "mongoose";
+
+router.post("/", (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body.type);
+  const todo = new Todos({
+    name: req.body.name,
+    type: req.body.type,
+  });
+  todo
+    .save()
+    .then((result: unknown): void => {
+      res.status(200).json({ message: "created succesfully", result });
+    })
+    .catch((err: unknown): void => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  Todos.find()
+    .exec()
+    .then((docs: any): void => {
+      console.log(docs);
+      const response = {
+        count: docs.length,
+        product: docs.map((doc: any) => {
+          return {
+            name: doc.name,
+            _id: doc._id,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((err: any) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+router.put("/:Id", (req: Request, res: Response, next: NextFunction) => {
+  const id: string = req.params.Id;
+  console.log(req.body.name);
+  Todos.findByIdAndUpdate(
+    req.params.Id,
+    { name: req.body.name },
+    { new: true }
+  );
+  res.json({ message: "Updated Todos sucessfully", id });
+});
+
+router.delete(
+  "/:todosId",
+  (req: Request, res: Response, next: NextFunction) => {
+    const id: string = req.params.todosId;
+    // const name:string=req.params.name
+    Todos.deleteOne({ _id: id })
+      .exec()
+      .then(() => {
+        res.status(200).json({
+          message: `Todos Deleted sucessfully ${id}`,
+        });
+      })
+      .catch((err: unknown): void => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  }
+);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ import SubCategory from "./api/models/subcatogry";
 import subcategoryRouter from "./api/routes/subcategory.route";
 import Todos from "./api/models/todos.model";
 import categoryRouter from "./api/routes/category.route";
-
+import todosRoute from "./api/routes/todos.route";
 mongoose
   .connect(
     "mongodb+srv://dawood9743:task123@tasknew.tbatzag.mongodb.net/?retryWrites=true&w=majority&appName=Tasknew"
@@ -30,93 +30,9 @@ import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//Complete api in app.ts start here for subcategory
-
-// Subcategory routes  end here
-
-// ||||||||||||||||||||||||||||||
-/////////////////__________________||||||||
-// Routing start for categoryRouter
-
-// routing ends for Category
-// ///////////////////___________-//////////////
-///////////////////////////////////////////////
-// Routing start for TodosStart here
-
-app.post("/todos", (req: Request, res: Response, next: NextFunction): void => {
-  console.log(req.body.type);
-  const todo = new Todos({
-    name: req.body.name,
-    type: req.body.type,
-  });
-  todo
-    .save()
-    .then((result: unknown): void => {
-      res.status(200).json({ message: "created succesfully", result });
-    })
-    .catch((err: unknown): void => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
-
-app.get("/todos", (req: Request, res: Response, next: NextFunction) => {
-  Todos.find()
-    .exec()
-    .then((docs: any): void => {
-      console.log(docs);
-      const response = {
-        count: docs.length,
-        product: docs.map((doc: result) => {
-          return {
-            name: doc.name,
-            _id: doc._id,
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err: any) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
-app.put("/todos/:Id", (req: Request, res: Response, next: NextFunction) => {
-  const id: string = req.params.Id;
-  console.log(req.body.name);
-  Todos.findByIdAndUpdate(
-    req.params.Id,
-    { name: req.body.name },
-    { new: true }
-  );
-  res.json({ message: "Updated Todos sucessfully", id });
-});
-
-app.delete(
-  "/todos/:todosId",
-  (req: Request, res: Response, next: NextFunction) => {
-    const id: string = req.params.todosId;
-    // const name:string=req.params.name
-    Todos.deleteOne({ _id: id })
-      .exec()
-      .then(() => {
-        res.status(200).json({
-          message: `Todos Deleted sucessfully ${id}`,
-        });
-      })
-      .catch((err: unknown): void => {
-        console.log(err);
-        res.status(500).json({ error: err });
-      });
-  }
-);
-
-// Routing ends here for Todos here
-
-// Routing start here
-
 app.use("/category", categoryRouter);
 app.use("/subcategory", subcategoryRouter);
+app.use("/todos", todosRoute);
 // app.use("/subcategory", subcategoryRouter);
 app.use((req: Request, res: Response, next: NextFunction) => {
   const error = new Error("Not Found");
